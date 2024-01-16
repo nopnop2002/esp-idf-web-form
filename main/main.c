cff698d3e4a8eedad4a613b82ae57cb062e99ecb
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-01-16 17:11:34 +0900

    Changed WiFi initialization

--- main/main.c
+++ main/main.c
@@ -43,8 +43,7 @@ static int s_retry_num = 0;
 QueueHandle_t xQueueHttp;
 
 
-static void event_handler(void* arg, esp_event_base_t event_base,
-								int32_t event_id, void* event_data)
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
 	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
 		esp_wifi_connect();
@@ -119,7 +118,16 @@ void wifi_init_sta()
 	wifi_config_t wifi_config = {
 		.sta = {
 			.ssid = CONFIG_ESP_WIFI_SSID,
-			.password = CONFIG_ESP_WIFI_PASSWORD
+			.password = CONFIG_ESP_WIFI_PASSWORD,
+			/* Setting a password implies station will connect to all security modes including WEP/WPA.
+			 * However these modes are deprecated and not advisable to be used. Incase your Access point
+			 * doesn't support WPA2, these mode can be enabled by commenting below line */
+			.threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+			.pmf_cfg = {
+				.capable = true,
+				.required = false
+			},
 		},
 	};
 	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
@@ -127,24 +135,23 @@ void wifi_init_sta()
 	ESP_ERROR_CHECK(esp_wifi_start() );
 
 	ESP_LOGI(TAG, "wifi_init_sta finished.");
-	ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
-			 CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+		WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+		pdFALSE,
+		pdFALSE,
+		portMAX_DELAY);
 
-	// wait for IP_EVENT_STA_GOT_IP
-	while(1) {
-		/* Wait forever for WIFI_CONNECTED_BIT to be set within the event group.
-		 Clear the bits beforeexiting. */
-		EventBits_t uxBits = xEventGroupWaitBits(s_wifi_event_group,
-			WIFI_CONNECTED_BIT, /* The bits within the event group to waitfor. */
-			pdTRUE,			/* WIFI_CONNECTED_BIT should be cleared before returning. */
-			pdFALSE,			/* Don't waitfor both bits, either bit will do. */
-			portMAX_DELAY);/* Wait forever. */
-		if ( ( uxBits & WIFI_CONNECTED_BIT ) == WIFI_CONNECTED_BIT ){
-			ESP_LOGI(TAG, "WIFI_CONNECTED_BIT");
-			break;
-		}
+	/* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+	 * happened. */
+	if (bits & WIFI_CONNECTED_BIT) {
+		ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else if (bits & WIFI_FAIL_BIT) {
+		ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else {
+		ESP_LOGE(TAG, "UNEXPECTED EVENT");
 	}
-	ESP_LOGI(TAG, "Got IP Address.");
 }
 
 void initialise_mdns(void)
